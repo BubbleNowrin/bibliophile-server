b9d4581a28d6a45362dc84eac58776608d7b567b
NowrinIslamMim <nowrin02@gmail.com>
2022-11-24 12:29:09 +0600

    query added to know if the user exists

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-const app = express();
+
 
 
 //midlleware
@@ -13,11 +14,39 @@ app.use(cors());
 
 //database connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ng69xjx.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
     res.send('bibliophile server running');
 })
+
+async function run() {
+    try {
+        //collections
+        const usersCollection = client.db('bibliophile').collection('users');
+
+        //add users to database
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const email = user.email;
+            const query = {
+                email: email
+            }
+            const savedUser = await usersCollection.findOne(query);
+            if (!savedUser) {
+                const result = await usersCollection.insertOne(user);
+                res.send(result);
+            }
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log())
+
+
 
 app.listen(port, () => {
     console.log(`server running on ${port}`);
